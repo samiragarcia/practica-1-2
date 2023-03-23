94c6ca5d9424b7d1ea1581c21a4aafe6ec1a17f2
samira garcia <samira.garcia95@gmail.com>
2023-03-23 19:01:00 -0300

    segundo commit

---
+++ algoritmo.js
@@ -0,0 +1,21 @@
+document.querySelector("body").innerHTML = ` 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/z95mZVUcJ-E" title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    <p> video tutorial de youtube del canal soy dalto </p>
+
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0167132768424!2d-58.38375908477038!3d-34.60373888045957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4aa9f0a6da5edb%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses-419!2sar!4v1679604337119!5m2!1ses-419!2sar"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <p> como llegar al obelisco </p>
+
+    <iframe style="border-radius:12px"
+        src="https://open.spotify.com/embed/track/7MXVkk9YMctZqd1Srtv4MB?utm_source=generator" width="100%" height="352"
+        frameBorder="0" allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <p> musica pop </p>
+
+        <iframe height="500px" width="1000px" src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+        <p> pagina de mercadolibre en mi pagina web </p> `
